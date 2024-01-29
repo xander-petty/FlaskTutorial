fe209aff0a89f73b3e7c7d1b5ae3545247eb2cbe
Xander <xander.petty@icloud.com>
2024-01-29 14:08:23 -0600

    fixed it

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/findhost', methods=['POST'])
+def findhost():
+    data = request.get_json()
+    print(data)
+    name = data['dns_name']
+    return render_template('findhost.html', name=name)
 
 if __name__ == '__main__':
     app.run()
